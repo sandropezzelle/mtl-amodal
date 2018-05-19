dc55da40e138dfc411ff97655d6af27c296d117f
Claudio Greco <claudio.greco@unitn.it>
2018-05-20 00:50:55 +0200

    Fixed script.

--- train_quant_lstm_model.py
+++ train_quant_lstm_model.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
     learning_rate = 0.001
     batch_size = 32
     embeddings = '/mnt/povobackup/clic/sandro.pezzelle/corpus-and-vectors/GoogleNews-vectors-negative300.txt'
-    weights = "best_msl_lstm_model/best-weight-model.hdf5"
+    weights = "best_quant_lstm_model/best-weight-model.hdf5"
 
     for n, dtp in enumerate(tr_inp):
         pad = np.zeros((25, 50))
