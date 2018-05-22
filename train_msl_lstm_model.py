e8b2b4e5ab43e286381518d9f1f61acb36cb4308
Claudio Greco <claudio.greco@unitn.it>
2018-05-22 16:41:48 +0200

    Improved script.

--- train_msl_lstm_model.py
+++ train_msl_lstm_model.py
@@ -1,7 +1,9 @@
 import argparse
 import os
 import pickle
+import warnings
 
+import numpy as np
 from keras.callbacks import ModelCheckpoint
 
 import msl_lstm_model
@@ -51,9 +53,65 @@ if __name__ == "__main__":
         v_q_out = valid["v_q_out"]
         v_r_out = valid["v_r_out"]
 
-    model = msl_lstm_model.MSLLSTMModel(embeddings_filename, token2id).build()
+    embeddings_index = {}
+    with open(args.embeddings_filename) as in_file:
+        for line in in_file:
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
 
-    checkpoint = ModelCheckpoint(weights_filename, monitor="val_loss", verbose=1, save_best_only=True, mode="min")
+    embedding_matrix = np.zeros((len(token2id) + 1, 300))
+    for word, i in token2id.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+    model = msl_lstm_model.MSLLSTMModel(embedding_matrix, token2id).build()
+
+
+    class MyModelCheckpoint(ModelCheckpoint):
+        def __init__(self, *args, **kwargs):
+            super(MyModelCheckpoint, self).__init__(*args, **kwargs)
+
+        def on_epoch_end(self, epoch, logs=None):
+            logs = logs or {}
+            self.epochs_since_last_save += 1
+            if self.epochs_since_last_save >= self.period:
+                self.epochs_since_last_save = 0
+                filepath = self.filepath.format(epoch=epoch + 1, **logs)
+                self.filepath = filepath
+                if self.save_best_only:
+                    current = logs.get(self.monitor)
+                    if current is None:
+                        warnings.warn('Can save best model only with %s available, '
+                                      'skipping.' % (self.monitor), RuntimeWarning)
+                    else:
+                        if self.monitor_op(current, self.best):
+                            if self.verbose > 0:
+                                print('\nEpoch %05d: %s improved from %0.5f to %0.5f,'
+                                      ' saving model to %s'
+                                      % (epoch + 1, self.monitor, self.best,
+                                         current, filepath))
+                            self.best = current
+                            if self.save_weights_only:
+                                self.model.save_weights(filepath, overwrite=True)
+                            else:
+                                self.model.save(filepath, overwrite=True)
+                        else:
+                            if self.verbose > 0:
+                                print('\nEpoch %05d: %s did not improve' %
+                                      (epoch + 1, self.monitor))
+                else:
+                    if self.verbose > 0:
+                        print('\nEpoch %05d: saving model to %s' % (epoch + 1, filepath))
+                    if self.save_weights_only:
+                        self.model.save_weights(filepath, overwrite=True)
+                    else:
+                        self.model.save(filepath, overwrite=True)
+
+
+    checkpoint = MyModelCheckpoint(weights_filename, monitor="val_loss", verbose=1, save_best_only=True, mode="min")
     hist = model.fit(
         dataset_tr,
         tr_m_out,
@@ -62,8 +120,9 @@ if __name__ == "__main__":
         validation_data=(dataset_v, v_m_out),
         callbacks=[checkpoint]
     )
+    print(checkpoint.filepath)
 
-    best_model = msl_lstm_model.MSLLSTMModel(embeddings_filename, token2id).build()
+    best_model = msl_lstm_model.MSLLSTMModel(embedding_matrix, token2id).build()
     best_model.load_weights(weights_filename)
     scores = best_model.evaluate(dataset_t, t_m_out, batch_size=args.batch_size)
     print("%s: %.4f%%" % (model.metrics_names[1], scores[1] * 100))
