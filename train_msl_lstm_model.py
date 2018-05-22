5aec8d77e1d3001403579b648e128260423cdffa
Claudio Greco <claudio.greco@unitn.it>
2018-05-22 16:06:52 +0200

    Improved script.

--- train_msl_lstm_model.py
+++ train_msl_lstm_model.py
@@ -1,213 +1,69 @@
-import sys
-from random import shuffle
+import argparse
+import os
+import pickle
 
-import numpy as np
 from keras.callbacks import ModelCheckpoint
-from keras.preprocessing.sequence import pad_sequences
 
 import msl_lstm_model
-import utils
 
-data_path = ''
-ratios = {}
-r_dict = {}
-
-
-def load_people(target_index, non_target_index):
-    target = data_path + '1900_133.txt'
-    non_target = data_path + '1700_133.txt'
-
-    with open(target, 'r') as targx:
-        reader = [line.split('\t')[3] for line in targx]
-
-    with open(non_target, 'r') as ntargx:
-        nreader = [line.split('\t')[3] for line in ntargx]
-
-    people = []
-
-    for idx in target_index:
-        if idx is not '':
-            i = int(idx)
-            myperson = reader[i].split()[0:50]
-            people.append(myperson)
-
-    for idx in non_target_index:
-        if idx is not '':
-            j = int(idx)
-            myperson = nreader[j].split()[0:50]
-            people.append(myperson)
-
-    pad = 25 - int(len(people))
-
-    for i in range(pad):
-        people.append([])
-
-    shuffle(people)
-
-    return people
-
-
-def read_data(data_path):
-    """
-    it reads train/validation/test files
-    """
-    tr = data_path + 'train_shuf.txt'
-    v = data_path + 'val_shuf.txt'
-    tst = data_path + 'test_shuf.txt'
-    return tr, v, tst
-
-
-def load_data(split):
-    """
-    for a given list of paths, it reads the image,
-    prepares it for input and it calculates the target value
-    """
-    with open(split, 'r') as splitfile:
-        reader = [line.split() for line in splitfile]
-        size = int(len(reader))
-        inp = [[] for _ in range(size)]
-        m_out = np.zeros((size, 3))
-        q_out = np.zeros((size, 9))
-        r_out = np.zeros((size, 17))
-        count = 0
-
-    with open(split, 'r') as splitfile:
-        for n, line in enumerate(splitfile):
-            line2 = line.split("', ")[2][1:]
-            target = line2.split("], [")[0]
-            target_arr = target.split(", ")
-            non_target = line2.split("], [")[1].strip("])']\n")
-            non_target_arr = non_target.split(", ")
-            cat = line[2:4]
-
-            for i in range(9):
-                q_out[count][i] = ratios[cat][str(i)]
-
-            r_out[count][r_dict[cat]] = 1.0
-
-            if cat[1] == 'Y' or cat[0] == 'X':
-                if cat[1] == 'Y':
-                    ratio_val = 0.0
-                else:
-                    ratio_val = 1.0
-            else:
-                ratio_val = float(cat[0]) / (float(cat[1]) + float(cat[0]))
-
-            if ratio_val < 0.5:
-                m_out[count][0] = 1.0
-
-            if ratio_val == 0.5:
-                m_out[count][1] = 1.0
-
-            if ratio_val > 0.5:
-                m_out[count][2] = 1.0
-
-            inpl = load_people(target_arr, non_target_arr)
-            inp[count].append(inpl)
-            count += 1
-
-        return inp, m_out, q_out, r_out
-
-
-def create_ratio_dict(ratios):
-    count = 0
-    r = sorted(ratios.keys())
-    for i in range(len(r)):
-        r_dict[r[i]] = count
-        count += 1
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     it reads the parameters,
     initializes the hyperparameters,
     preprocesses the input,
     trains the model
     """
-    repository_path = sys.argv[1]
-    data_path = sys.argv[2]
-    tr, v, tst = read_data(data_path)
+    preprocessed_dataset_path = "lang_dataset/"
+    embeddings_filename = "/mnt/povobackup/clic/sandro.pezzelle/corpus-and-vectors/GoogleNews-vectors-negative300.txt"
+    weights_filename = "best_models/msl_lstm_model-{epoch:02d}-{val_loss:.4f}-{val_acc:.4f}.hdf5"
+    predictions_filename = "best_models/msl_lstm_model-{epoch:02d}-{val_loss:.4f}-{val_acc:.4f}.predictions"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--preprocessed_dataset_path", type=str, default=preprocessed_dataset_path)
+    parser.add_argument("--embeddings_filename", type=str, default=embeddings_filename)
+    parser.add_argument("--weights_filename", type=str, default=weights_filename)
+    parser.add_argument("--num_epochs", type=int, default=100)
+    parser.add_argument("--batch_size", type=int, default=32)
+    args = parser.parse_args()
 
-    ratios = utils.read_qprobs(repository_path)
-    create_ratio_dict(ratios)
+    with open(os.path.join(args.preprocessed_dataset_path, "index.pkl"), mode="rb") as in_file:
+        index = pickle.load(in_file)
+        token2id = index["token2id"]
+        id2token = index["id2token"]
 
-    tr_inp, tr_m_out, tr_q_out, tr_r_out = load_data(tr)
-    v_inp, v_m_out, v_q_out, v_r_out = load_data(v)
-    t_inp, t_m_out, t_q_out, t_r_out = load_data(tst)
+    with open(os.path.join(args.preprocessed_dataset_path, "train.pkl"), mode="rb") as in_file:
+        train = pickle.load(in_file)
+        dataset_tr = train["dataset_tr"]
+        tr_m_out = train["tr_m_out"]
+        tr_q_out = train["tr_q_out"]
+        tr_r_out = train["tr_r_out"]
 
-    token2id = {}
-    id2token = {}
-    dataset_tr, dataset_v, dataset_t = [], [], []
+    with open(os.path.join(args.preprocessed_dataset_path, "test.pkl"), mode="rb") as in_file:
+        test = pickle.load(in_file)
+        dataset_t = train["dataset_t"]
+        t_m_out = train["t_m_out"]
+        t_q_out = train["t_q_out"]
+        t_r_out = train["t_r_out"]
 
-    num_epochs = 100
-    learning_rate = 0.001
-    batch_size = 32
-    embeddings = '/mnt/povobackup/clic/sandro.pezzelle/corpus-and-vectors/GoogleNews-vectors-negative300.txt'
-    weights = "best_msl_lstm_model/best-weight-model.hdf5"
+    with open(os.path.join(args.preprocessed_dataset_path, "valid.pkl"), mode="rb") as in_file:
+        valid = pickle.load(in_file)
+        dataset_v = train["dataset_v"]
+        v_m_out = train["v_m_out"]
+        v_q_out = train["v_q_out"]
+        v_r_out = train["v_r_out"]
 
-    for n, dtp in enumerate(tr_inp):
-        pad = np.zeros((25, 50))
-        for i in dtp:
-            authors = []
-            for nn, el in enumerate(i):
-                author = []
-                for token in el:
-                    if not token in token2id:
-                        i = len(token2id) + 1
-                        token2id[token] = i
-                        id2token[i] = token
-                    author.append(token2id[token])
-                authors.append(author)
-            authors = pad_sequences(authors, padding='post')
-            dataset_tr.append(authors)
-    dataset_tr = np.array(dataset_tr)
+    model = msl_lstm_model.MSLLSTMModel(embeddings_filename, token2id).build()
 
-    for n, dtp in enumerate(v_inp):
-        pad = np.zeros((25, 50))
-        for i in dtp:
-            authors = []
-            for nn, el in enumerate(i):
-                author = []
-                for token in el:
-                    if not token in token2id:
-                        i = len(token2id) + 1
-                        token2id[token] = i
-                        id2token[i] = token
-                    author.append(token2id[token])
-                authors.append(author)
-            authors = pad_sequences(authors, padding='post')
-            dataset_v.append(authors)
-    dataset_v = np.array(dataset_v)
-
-    for n, dtp in enumerate(t_inp):
-        pad = np.zeros((25, 50))
-        for i in dtp:
-            authors = []
-            for nn, el in enumerate(i):
-                author = []
-                for token in el:
-                    if not token in token2id:
-                        i = len(token2id) + 1
-                        token2id[token] = i
-                        id2token[i] = token
-                    author.append(token2id[token])
-                authors.append(author)
-            authors = pad_sequences(authors, padding='post')
-            dataset_t.append(authors)
-    dataset_t = np.array(dataset_t)
-
-    model = msl_lstm_model.MSLLSTMModel(embeddings, token2id).build()
-
-    checkpoint = ModelCheckpoint(weights, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+    checkpoint = ModelCheckpoint(weights_filename, monitor="val_loss", verbose=1, save_best_only=True, mode="min")
     hist = model.fit(
         dataset_tr,
         tr_m_out,
-        batch_size=batch_size,
-        epochs=num_epochs,
+        batch_size=args.batch_size,
+        epochs=args.num_epochs,
         validation_data=(dataset_v, v_m_out),
         callbacks=[checkpoint]
     )
 
-    best_model = msl_lstm_model.MSLLSTMModel(embeddings, token2id).build()
-    best_model.load_weights(weights)
-    print(best_model.evaluate(dataset_t, t_m_out, batch_size=batch_size))
+    best_model = msl_lstm_model.MSLLSTMModel(embeddings_filename, token2id).build()
+    best_model.load_weights(weights_filename)
+    scores = best_model.evaluate(dataset_t, t_m_out, batch_size=args.batch_size)
+    print("%s: %.4f%%" % (model.metrics_names[1], scores[1] * 100))
