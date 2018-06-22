bfc5398288ec6a54ea70a506661499e4e33a78e6
Claudio Greco <claudio.greco@unitn.it>
2018-06-22 11:25:30 +0200

    Fixed and improved scripts.

--- train_vision_to_lang_model.py
+++ train_vision_to_lang_model.py
@@ -1,21 +1,15 @@
 import argparse
 import os
 import pickle
-import unicodecsv as csv
+
 import numpy as np
-import pandas as pd
+from keras.callbacks import EarlyStopping
 
 import multitask_lang_model
 import multitask_vision_model
 from utils import MyModelCheckpoint
 
 if __name__ == '__main__':
-    """
-    it reads the parameters,
-    initializes the hyperparameters,
-    preprocesses the input,
-    trains the model
-    """
     preprocessed_dataset_path = "lang_dataset/"
     embeddings_filename = "/mnt/povobackup/clic/sandro.pezzelle/corpus-and-vectors/GoogleNews-vectors-negative300.txt"
     vision_weights_filename = "/mnt/povobackup/clic/sandro.pezzelle/model_weights_final/multi-task-prop/weight.best.hdf5"
@@ -51,17 +45,6 @@ if __name__ == '__main__':
         dataset_tr_names = train["dataset_tr_names"]
         dataset_tr_years = train["dataset_tr_years"]
 
-    test_filename = os.path.join(args.preprocessed_dataset_path, "test.pkl")
-    print("Loading filename: {}".format(test_filename))
-    with open(os.path.join(args.preprocessed_dataset_path, "test.pkl"), mode="rb") as in_file:
-        test = pickle.load(in_file)
-        dataset_t = test["dataset_t"]
-        t_m_out = test["t_m_out"]
-        t_q_out = test["t_q_out"]
-        t_r_out = test["t_r_out"]
-        dataset_t_names = train["dataset_t_names"]
-        dataset_t_years = train["dataset_t_years"]
-
     valid_filename = os.path.join(args.preprocessed_dataset_path, "valid.pkl")
     print("Loading filename: {}".format(valid_filename))
     with open(os.path.join(args.preprocessed_dataset_path, "valid.pkl"), mode="rb") as in_file:
@@ -70,8 +53,8 @@ if __name__ == '__main__':
         v_m_out = valid["v_m_out"]
         v_q_out = valid["v_q_out"]
         v_r_out = valid["v_r_out"]
-        dataset_v_names = train["dataset_v_names"]
-        dataset_v_years = train["dataset_v_years"]
+        dataset_v_names = valid["dataset_v_names"]
+        dataset_v_years = valid["dataset_v_years"]
 
     print("Loading filename: {}".format(args.embeddings_filename))
     embeddings_index = {}
@@ -94,73 +77,12 @@ if __name__ == '__main__':
     lang_model = multitask_lang_model.MultitaskLangModel(embedding_matrix, token2id, multitask_vision_model=vision_model).build()
 
     checkpoint = MyModelCheckpoint(args.lang_weights_filename, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3, mode='min')
     hist = lang_model.fit(
         dataset_tr,
         [tr_m_out, tr_q_out, tr_r_out],
         batch_size=args.batch_size,
         epochs=args.num_epochs,
         validation_data=(dataset_v, [v_m_out, v_q_out, v_r_out]),
-        callbacks=[checkpoint]
+        callbacks=[checkpoint, early_stopping]
     )
-
-    print("Evaluating model...")
-    best_model = multitask_lang_model.MultitaskLangModel(embedding_matrix, token2id).build()
-    best_model.load_weights(checkpoint.best_saved_filename)
-    scores = best_model.evaluate(dataset_t, [t_m_out, t_q_out, t_r_out], batch_size=args.batch_size)
-    for i in range(len(scores)):
-        print("%s: %.4f%%" % (best_model.metrics_names[i], scores[i]))
-
-    predictions = best_model.predict(dataset_t, batch_size=args.batch_size)
-
-    y_pred_msl = np.argmax(predictions[0], axis=1)
-    y_valarr_msl = np.argmax(t_m_out, axis=1)
-    pd.crosstab(y_valarr_msl, y_pred_msl, margins=True).to_csv(checkpoint.best_saved_filename.replace(".hdf5", ".confusion_msl"))
-
-    y_pred_prop = np.argmax(predictions[2], axis=1)
-    y_valarr_prop = np.argmax(t_r_out, axis=1)
-    pd.crosstab(y_valarr_prop, y_pred_prop, margins=True).to_csv(checkpoint.best_saved_filename.replace(".hdf5", ".confusion_prop"))
-
-    with open(checkpoint.best_saved_filename.replace(".hdf5", ".predictions_quant"), mode="w") as out_file:
-        for i in range(3400):
-            for j in range(9):
-                out_file.write(str(predictions[1][i][j]) + '\t')
-            out_file.write('\n')
-            for j in range(9):
-                out_file.write(str(t_q_out[i][j]) + '\t')
-            out_file.write('\n')
-
-    with open(checkpoint.best_saved_filename.replace(".hdf5", ".predictions"), mode="w") as out_file:
-        writer = csv.writer(out_file, delimiter="\t", encoding="utf-8")
-        writer.writerow(
-            [
-                "names",
-                "years",
-                "t_m_out",
-                "t_q_out",
-                "t_r_out",
-                "t_pred_m_out",
-                "t_pred_q_out",
-                "t_pred_r_out"
-            ]
-        )
-        for i, scenario in enumerate(dataset_t):
-            formatted_names = ", ".join(dataset_t_names[i])
-            formatted_years = ", ".join(dataset_t_years[i])
-            formatted_t_m_out = id2m_out[tuple(t_m_out[i])]
-            formatted_t_q_out = str(t_q_out[i])
-            formatted_t_r_out = id2r_out[tuple(t_r_out[i])]
-            formatted_t_pred_m_out = y_pred_msl[i]
-            formatted_t_pred_q_out = str(predictions[1][i])
-            formatted_t_pred_r_out = y_pred_prop[i]
-            writer.writerow(
-                [
-                    formatted_names,
-                    formatted_years,
-                    formatted_t_m_out,
-                    formatted_t_q_out,
-                    formatted_t_r_out,
-                    formatted_t_pred_m_out,
-                    formatted_t_pred_q_out,
-                    formatted_t_pred_r_out
-                ]
-            )
